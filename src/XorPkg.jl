d4538c357426d4f08f90766077b12c287207ecfd
Krishna Praneet Gudipaty <kgudipaty@umass.edu>
2023-05-04 12:02:43 -0400

    Added tests

--- src/XorPkg.jl
+++ src/XorPkg.jl
@@ -1,22 +1,22 @@
 module XorPkg
 using Base.Threads
 
-export forXor, broadcastXor, threadsXor
+export forXor!, broadcastXor!, threadsXor!
 
 
-function forXor(output::Vector{UInt64}, ina::Vector{UInt64}, inb::Vector{UInt64})
+function forXor!(output::Vector{UInt64}, ina::Vector{UInt64}, inb::Vector{UInt64})
   @assert length(output) == length(ina) == length(inb)
   for i in eachindex(output)
       output[i] = ina[i] ⊻ inb[i]
   end
 end
 
-function broadcastXor(output::Vector{UInt64}, ina::Vector{UInt64}, inb::Vector{UInt64})
+function broadcastXor!(output::Vector{UInt64}, ina::Vector{UInt64}, inb::Vector{UInt64})
   @assert length(output) == length(ina) == length(inb)
-  output .= ina .⊻ inb
+  output = ina .⊻ inb
 end
 
-function threadsXor(output::Vector{UInt64}, ina::Vector{UInt64}, inb::Vector{UInt64})
+function threadsXor!(output::Vector{UInt64}, ina::Vector{UInt64}, inb::Vector{UInt64})
   @assert length(output) == length(ina) == length(inb)
   @threads for i in eachindex(output)
     output[i] = ina[i] ⊻ inb[i]
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,19 @@ using BenchmarkTools
 
 @testset "XorPkg.jl" begin
     # Write your tests here.
+    output = zeros(UInt64, 10)
+    ina = rand(UInt64, 10)
+    inb = rand(UInt64, 10)
+    forXor!(output, ina, inb)
+
+    @test output == ina .⊻ inb 
 
     output = zeros(UInt64, 10)
     ina = rand(UInt64, 10)
     inb = rand(UInt64, 10)
-
-    forXor(output, ina, inb)
+    threadsXor!(output, ina, inb)
+    
     @test output == ina .⊻ inb 
 end
+
+
