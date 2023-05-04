a22a1147b0de17d880e3cf6716eed6425e2d7975
Krishna Praneet Gudipaty <kgudipaty@umass.edu>
2023-05-04 08:51:54 -0400

    Added functions in XorPkg.jl

--- src/XorPkg.jl
+++ src/XorPkg.jl
@@ -1,9 +1,26 @@
 module XorPkg
+using Base.Threads
 
-# Write your package code here.
-export forXor
-export broadcastXor
-export threadsXor
-include("functions.jl")
+export forXor, broadcastXor, threadsXor
+
+
+function forXor(output, ina, inb)
+  @assert length(output) == length(ina) == length(inb)
+  for i in eachindex(output)
+      output[i] = ina[i] ⊻ inb[i]
+  end
+end
+
+function broadcastXor(output, ina, inb)
+  @assert length(output) == length(ina) == length(inb)
+  output .= ina .⊻ inb
+end
+
+function threadsXor(output, ina, inb)
+  @assert length(output) == length(ina) == length(inb)
+  @threads for i in eachindex(output)
+    output[i] = ina[i] ⊻ inb[i]
+  end
+end
 
 end
